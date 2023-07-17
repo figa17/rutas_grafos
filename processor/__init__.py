8872dbe5cf7830dd0efa748a3c6028a374bc4c44
Felipe González <fel.alfaro@gmail.com>
2023-07-17 01:55:06 -0400

    delete unused package

--- processor/__init__.py
+++
@@ -1,1 +0,0 @@
-from .route_processor import RouteProcessor
--- processor/abstract_procesor.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from model import RequestSolver, ResultSolver
-
-
-class AbstractProcessor(ABC):
-
-    @abstractmethod
-    def process(self, request: RequestSolver) -> ResultSolver:
-        pass
